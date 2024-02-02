6a0529e5df4e40ae722a8c436e493de6bef54755
Kazi Aynun Nahin Dip <103853468+Nahian1269@users.noreply.github.com>
2024-02-02 15:54:58 +0600

    js

---
+++ century.js
@@ -0,0 +1,11 @@
+// Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+let count = 200 ; 
+let  stat ; 
+
+for( let i = 1; i<=count; i++){
+    if(i===100){
+        console.log(i);
+        break;
+    }
+}
